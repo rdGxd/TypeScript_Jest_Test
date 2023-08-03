d5c45e02204251ab77f3f014c83049e4790d62ac
Rodrigo Silva <rdg.hora1@gmail.com>
2023-08-03 13:27:02 -0300

    Introdução JEST

---
+++ src/services/persistency.spec.ts
@@ -0,0 +1,16 @@
+// Primeiro parâmetro é a descrição e depois a função
+
+describe('Testando alguma coisa', () => {
+  it(`should return one`, () => {
+    const number = 1;
+    // negando
+    expect(number).not.toBe(1);
+  });
+});
+
+describe('Testando outra coisa', () => {
+  test(`should return Rodrigo`, () => {
+    const nome = 'Rodrigo';
+    expect(nome).toBe('Rodrigo');
+  });
+});
