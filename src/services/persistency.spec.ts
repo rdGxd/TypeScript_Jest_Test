479f18a7eaa61b2b9dfec06f9c65fce782a62d22
Rodrigo Silva <rdg.hora1@gmail.com>
2023-08-05 12:06:14 -0300

    Primeiro teste com spyOn
    Primeiro teste com spyOn

--- src/services/persistency.spec.ts
+++ src/services/persistency.spec.ts
@@ -1,16 +1,43 @@
 // Primeiro parâmetro é a descrição e depois a função
 
-describe('Testando alguma coisa', () => {
-  it(`should return one`, () => {
-    const number = 1;
-    // negando
-    expect(number).not.toBe(2);
-  });
-});
+import { Persistency } from './persistency';
 
-describe('Testando outra coisa', () => {
-  test(`should return Rodrigo`, () => {
-    const nome = 'Rodrigo';
-    expect(nome).toBe('Rodrigo');
+describe('Persistency', () => {
+  // Limpando os mocks depois de cada teste
+  afterEach(() => jest.clearAllMocks());
+
+  // Esse teste precisa retorna void
+  it('should return undefined', () => {
+    // Sempre vamos chamar a classe que está sendo testada de SUT -> System under test
+    const sut = new Persistency();
+
+    // Eu espero que meu método não retorne nada
+    expect(sut.saveOrder()).toBeUndefined();
+  });
+
+  // Esse teste vai verificar se o console.log foi chamado
+  it('should call console.log once', () => {
+    // Sempre vamos chamar a classe que está sendo testada de SUT -> System under test
+    const sut = new Persistency();
+
+    // Jogando em uma variável uma coisa para o JEST observa
+    const consoleSpy = jest.spyOn(console, 'log');
+
+    // Verificando se o console.log foi chamado 1 vez
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  // Verificando qual o valor do console.log
+  it('should call console.log with "Pedido salvo com sucesso..."', () => {
+    // Sempre vamos chamar a classe que está sendo testada de SUT -> System under test
+    const sut = new Persistency();
+
+    // Jogando em uma variável uma coisa para o JEST observa
+    const consoleSpy = jest.spyOn(console, 'log');
+
+    // Verificando se o console.log foi chamado 1 vez
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso...');
   });
 });
--- src/services/persistency.ts
+++ src/services/persistency.ts
@@ -3,6 +3,6 @@ import { PersistencyProtocol } from '../classes/interfaces/persistency-protocol'
 export class Persistency implements PersistencyProtocol {
   // Salvando a ordem de pedido
   saveOrder(): void {
-    console.log('Pedido salvo com sucesso');
+    console.log('Pedido salvo com sucesso...');
   }
 }
