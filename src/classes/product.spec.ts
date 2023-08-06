ada429827ae899cb2e2ef1be87955c0a0cf91dd4
Rodrigo Silva <rdg.hora1@gmail.com>
2023-08-06 09:01:39 -0300

    Testando Product.ts
    Testando Product.ts

---
+++ src/classes/product.spec.ts
@@ -0,0 +1,22 @@
+// Primeiro parâmetro é a descrição e depois a função
+
+import { Product } from './product';
+
+const createSut = (name: string, price: number): Product => {
+  return new Product(name, price);
+};
+
+describe('Product', () => {
+  // Limpando os mocks depois de cada teste
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    // Sempre vamos chamar a classe que está sendo testada de SUT -> System under test
+    const sut = createSut('Camiseta', 49.9);
+
+    // Checando as propriedades
+    // expect(sut.name).toBe('Camiseta');
+    expect(sut).toHaveProperty('name', 'Camiseta');
+    expect(sut.price).toBeCloseTo(49.9);
+  });
+});
